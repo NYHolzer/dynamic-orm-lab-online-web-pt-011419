2d0fe32b64826dc025f530a179fc4b0dd0cc77d3
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-07 20:21:55 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -61,7 +61,7 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end 
   
-  def self.find_by(key: value)
+  def self.find_by(attributes)
     sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'"
     DB[:conn].execute(sql)
   end 
